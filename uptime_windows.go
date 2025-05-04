2ac1bff6107d53260cf56a970c7fd31f37a50d68
Michael Klose <m.klose@route4all.com>
2025-05-04 09:54:59 +0200

    Add Uptime Support for Windows

--- uptime_windows.go
+++ uptime_windows.go
@@ -3,4 +3,16 @@
 
 package main
 
-func systemUptime() string { return "n/a on Windows" }
+import (
+	"syscall"
+	"time"
+)
+
+// systemUptime returns the OS uptime as a rounded duration string on Windows.
+func systemUptime() string {
+	kernel32 := syscall.NewLazyDLL("kernel32.dll")
+	getTick := kernel32.NewProc("GetTickCount64")
+	ret, _, _ := getTick.Call()
+	ms := uint64(ret)
+	return (time.Duration(ms) * time.Millisecond).Round(time.Second).String()
+}
